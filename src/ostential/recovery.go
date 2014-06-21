326288d9be861f3cc221b444b683273bf3f28d9a
Roman <roman@ostrost.com>
2014-06-21 19:01:18 +0400

    less func calls after panic and recover()ing, not to show up in the stack

--- src/ostential/recovery.go
+++ src/ostential/recovery.go
@@ -7,11 +7,29 @@ import (
 
 type Recovery bool // true stands for production
 
-func(rc Recovery) Constructor(HANDLER http.Handler) http.Handler {
+func(RC Recovery) Constructor(HANDLER http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		defer func() {
-			if E := recover(); E != nil {
-				rc.recov(w, E)
+			if err := recover(); err != nil {
+				w.WriteHeader(panicstatuscode) // NB
+
+				var description string
+				if err, ok := err.(error); ok {
+					description = err.Error()
+				}
+				var stack string
+				if !RC { // if !production
+					sbuf := make([]byte, 4096 - len(panicstatustext) - len(description))
+					size := runtime.Stack(sbuf, false)
+					stack = string(sbuf[:size])
+				}
+				rctemplate.Execute(w, struct {
+					Title, Description, Stack string
+				}{
+					Title:       panicstatustext,
+					Description: description,
+					Stack:       stack,
+				})
 			}
 		}()
 		HANDLER.ServeHTTP(w, r)
@@ -20,28 +38,6 @@ func(rc Recovery) Constructor(HANDLER http.Handler) http.Handler {
 
 const panicstatuscode = http.StatusInternalServerError
 var   panicstatustext = statusLine(panicstatuscode)
-
-func (RC Recovery) recov(w http.ResponseWriter, E interface{}) {
-	w.WriteHeader(panicstatuscode) // NB
-
-	var description string
-	if err, ok := E.(error); ok {
-		description = err.Error()
-	}
-	var stack string
-	if !RC { // if !production
-		sbuf := make([]byte, 4096 - len(panicstatustext) - len(description))
-		size := runtime.Stack(sbuf, false)
-		stack = string(sbuf[:size])
-	}
-	rctemplate.Execute(w, struct {
-		Title, Description, Stack string
-	}{
-		Title:       panicstatustext,
-		Description: description,
-		Stack:       stack,
-	})
-}
 
 var rctemplate = template.Must(template.New("recovery.html").Parse(`
 <html>
