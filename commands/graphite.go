da52622c742bcc32a878d361c7fa57cc1f8f11a7
Roman <roman@ostrost.com>
2014-11-17 16:26:15 +0300

    option to send metrics to graphite: ostent --sendto-graphite server[:port]

---
+++ commands/graphite.go
@@ -0,0 +1,84 @@
+package commands
+
+import (
+	"flag"
+	"log"
+	"net"
+	"os"
+	"time"
+
+	"github.com/ostrost/ostent"
+	"github.com/ostrost/ostent/client"
+	"github.com/ostrost/ostent/types"
+	metrics "github.com/rcrowley/go-metrics"
+)
+
+type graphite struct {
+	logger     *loggerWriter
+	ServerAddr types.BindValue
+}
+
+func graphiteCommandLine(cli *flag.FlagSet) commandLineHandler {
+	gr := &graphite{
+		logger: &loggerWriter{
+			log.New(os.Stderr, "[ostent sendto-graphite] ", log.LstdFlags),
+		},
+		ServerAddr: types.NewBindValue(2003),
+	}
+	cli.Var(&gr.ServerAddr, "sendto-graphite", "Graphite server address")
+	return func() (atexitHandler, bool, error) {
+		if gr.ServerAddr.Host == "" {
+			return nil, false, nil
+		}
+		ostent.AddBackground(func() {
+			gc := &carbond{
+				logger:     gr.logger,
+				serveraddr: gr.ServerAddr.String(),
+				Client:     client.DefaultClient(types.Duration(time.Second)),
+			}
+			ostent.Register <- gc
+		})
+		return nil, false, nil
+	}
+}
+
+type carbond struct {
+	logger        *loggerWriter
+	serveraddr    string
+	conn          net.Conn
+	client.Client // expires() bool method
+	failing       bool
+}
+
+func (_ *carbond) CloseChans() {} // intentionally empty
+func (_ *carbond) Reload()     {} // intentionally empty
+
+func (_ *carbond) Push(*ostent.IndexUpdate) {} // TODO?
+
+func (cd *carbond) Tack() {
+	addr, err := net.ResolveTCPAddr("tcp", cd.serveraddr)
+	if err != nil {
+		cd.logger.Printf("Resolve Addr %s: %s\n", cd.serveraddr, err)
+		return
+	}
+	// go metrics.Graphite(ostent.Reg1s.Registry, 1*time.Second, "ostent", addr)
+	err = metrics.GraphiteOnce(metrics.GraphiteConfig{
+		DurationUnit: time.Nanosecond, // default, used(divided by thus must not be 0) with Timer metrics
+		Addr:         addr,
+		Registry:     ostent.Reg1s.Registry,
+		Prefix:       "ostent",
+	})
+	if err != nil {
+		if !cd.failing {
+			cd.failing = true
+			cd.logger.Printf("Sending: %s\n", err)
+		}
+	} else if cd.failing {
+		cd.failing = false
+		cd.logger.Printf("Recovered\n")
+	}
+}
+
+func init() {
+	AddCommandLine(graphiteCommandLine)
+}
