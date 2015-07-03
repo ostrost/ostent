b472a2de3a434062d84b945f143fc38409bf1c84
Roman <roman@ostrost.com>
2015-07-03 16:43:13 +0000

    test fix

--- templateutil/templatepipe/templatepipe_test.go
+++ templateutil/templatepipe/templatepipe_test.go
@@ -42,7 +42,7 @@ func TestEncurl(t *testing.T) {
 
 	v := Encurl(CurlyX, d, -1)
 	c := v.(Nota)["a"].(Nota)["b"].(Nota)["c"].([]map[string]Nota)[0]
-	if expected := "{DECL.z}"; c["z"].ToString() != expected {
+	if expected := "{DECL.z}"; c["z"].String() != expected {
 		t.Errorf("Encurl result mismatch: %q (expected %q)", c["z"], expected)
 	}
 	if z := v.(Nota)["a"].(Nota)["b"].(Nota)["z"].([]string); len(z) != 0 {
