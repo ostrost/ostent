0058063c1fe0e4b52a3b25f9dd78ba4fbdda288d
Roman <roman@ostrost.com>
2015-12-28 21:42:14 +0000

    gulp watch: probe gmake(1) presence, fallback to make(1)

--- gulpfile.watch.js
+++ gulpfile.watch.js
@@ -3,12 +3,11 @@ var child             = require('child_process'),
     argv              = require('yargs').argv;
 //  reload            = require('gulp-livereload');
 
-var makevars = {}, server;
+var server, makep = {_cmd: 'make'};
 
 function dup(std, proc) {
-  // return proc[std].on('data', function(d) { process[std].write(d.toString()); });
+  return proc[std].on('data', function(d) { process[std].write(d.toString()); });
   return proc[std].on('data', function(d) {
-    return process[std].write(d.toString());
     var lines = d.toString().split('\n');
     for (var i in lines) {
       util.log(lines[i]); // util is gulp-util
@@ -16,27 +15,37 @@ function dup(std, proc) {
   });
 }
 
-gulp.task('gmake dev', function() {
-  var make = child.spawn('gmake', '-C . dev'.split(' '));
-  dup('stdout', make);
-  dup('stderr', make);
+gulp.task('which gmake', function(cb) {
+  var proc = child.spawn('which', ['gmake']);
+  proc.stdout.on('end', cb);
+  proc.stdout.on('data', function() { makep._cmd = 'gmake'; });
+  dup('stderr', proc);
 });
 
-gulp.task('gmake print', function(cb) {
-  var make = child.spawn('gmake', 'print-package print-devpackagefiles'.split(' '));
-  make.stdout.on('end', cb);
-  make.stdout.on('data', function(d) {
-    var words = d.toString().split('=');
-    makevars[words[0]] = words[1].replace(/\n$/, '').split(' ');
+gulp.task('gmake dev', ['which gmake'], function() {
+  var proc = child.spawn(makep._cmd, '-C . dev'.split(' '));
+  dup('stdout', proc);
+  dup('stderr', proc);
+});
+
+gulp.task('gmake print', ['which gmake'], function(cb) {
+  var proc = child.spawn(makep._cmd, 'print-package print-devpackagefiles'.split(' '));
+  proc.stdout.on('end', cb);
+  proc.stdout.on('data', function(d) {
+    var lines = d.toString().split('\n');
+    for (var i in lines) {
+      var words = d.toString().split('=');
+      makep[words[0]] = words[1].replace(/\n$/, '').split(' ');
+    }
   });
-  dup('stderr', make);
+  dup('stderr', proc);
 });
 
 gulp.task('server build', function(cb) {
-  var build = child.spawn('go', ('get -v -race '+makevars.package[0]).split(' '));
-  build.stdout.on('end', cb);
-  dup('stdout', build);
-  dup('stderr', build);
+  var proc = child.spawn('go', ('get -v -race '+makep.package[0]).split(' '));
+  proc.stdout.on('end', cb);
+  dup('stdout', proc);
+  dup('stderr', proc);
 });
 
 gulp.task('server run', ['server build'], function() {
@@ -51,7 +60,7 @@ gulp.task('server run', ['server build'], function() {
         args.push(argv[i]);
       }
     }
-    server = child.spawn(makevars.package[0].replace(/.*\//, ''), args);
+    server = child.spawn(makep.package[0].replace(/.*\//, ''), args);
     dup('stdout', server);
     dup('stderr', server);
     // server.stdout.once('data', function() { reload.reload('/'); });
@@ -65,7 +74,7 @@ gulp.task('server run', ['server build'], function() {
 });
 
 gulp.task('server watch', function() {
-  gulp.watch(makevars.devpackagefiles.map(function(x) { return __dirname + '/' + x; }),
+  gulp.watch(makep.devpackagefiles.map(function(x) { return __dirname + '/' + x; }),
              ['server run']);
 });
 
